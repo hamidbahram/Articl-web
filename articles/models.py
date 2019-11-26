f150e89f9b12dc088f7b1caf9ef7826388827403
hamid_bh <hamidbahram2@gmail.com>
2019-11-26 18:08:27 +0330

    improve models & add update time & absolute url

--- articles/models.py
+++ articles/models.py
@@ -1,16 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 
 class Article(models.Model):
-    title  =  models.CharField(max_length=100)
-    slug   =  models.SlugField()
-    body   =  models.TextField()
-    date   =  models.DateTimeField(auto_now_add=True)
+    title            =  models.CharField(max_length=100)
+    slug             =  models.SlugField()
+    body             =  models.TextField()
+    date             =  models.DateTimeField(_('create date'), auto_now_add=True, auto_now=False)
+    updatedatetime   =  models.DateTimeField(_('update date'), auto_now_add=False, auto_now=True)
     # pip install pillow
-    thumb  =  models.ImageField(default='default.png', blank=True)
-    author =  models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    thumb            =  models.ImageField(default='default.png', blank=True)
+    author           =  models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                default=None,
+                                blank = True,
+                                null = True,
+                                verbose_name = _('Owner'),
+                                help_text = _('choose owner'))
+
+    class Meta:
+        verbose_name = _('Article')
+        verbose_name_plural = _('Articles')
 
     def __str__(self):
         #return self.title
@@ -19,3 +33,7 @@ class Article(models.Model):
     def snippet(self):
         #return self.body[:50]
         return "{}".format(self.body[:50]) + " ..."
+
+    def get_absolute_url(self):
+        return reverse("articles:detail", kwargs={"slug": self.slug})
+    
