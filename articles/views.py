fbdfe533df0f5fa0bab0c88bb1629f87fa23cb2b
hamid_bh <hamidbahram2@gmail.com>
2019-11-26 18:12:44 +0330

    create search

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Q
 from .models import Article
@@ -8,13 +8,25 @@ from . import forms
 
 # Create your views here.
 
+class SearchResultsView(ListView):
+    model = Article
+    template_name = 'articles/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Article.objects.filter(
+            Q(title__icontains = query) 
+        )
+        return object_list
+
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request,'articles/article_list.html', {'articles':articles})
 
 def article_detail(request, slug):
     #return HttpResponse(slug)
-    article = Article.objects.get(slug=slug)
+    # article = Article.objects.get(slug=slug)
+    article = get_object_or_404(Article, slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article})
 
 @login_required(login_url = "/accounts/login/")
