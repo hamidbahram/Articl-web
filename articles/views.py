94e9dd890a637377fb6be47c96840bae8c7a1eeb
hamid_bh <hamidbahram2@gmail.com>
2019-12-26 13:41:47 +0330

    create edit

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from django.shortcuts import render, redirect, get_object_or_404, Http404
-from django.http import HttpResponse
-from django.db.models import Q
-from .models import Article
-from . import forms
 from django.contrib import messages
+from django.db.models import Q
+from .forms import CreateArticle, EditArticle
+from .models import Article
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -22,12 +23,17 @@ class SearchResultsView(ListView):
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
+    # Pagination
+    paginator = Paginator(articles, 3) # Show 3 contacts per page
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+    
     return render(request,'articles/article_list.html', {'articles':articles})
 
-def article_detail(request, slug):
+def article_detail(request, id=None):
     #return HttpResponse(slug)
     # article = Article.objects.get(slug=slug)
-    article = get_object_or_404(Article, slug=slug)
+    article = get_object_or_404(Article, id=id)
     return render(request, 'articles/article_detail.html', {'article':article})
 
 @login_required(login_url = "/accounts/login/")
@@ -35,27 +41,40 @@ def article_create(request):
     if not request.user.is_staff or not request.user.is_superuser:
         raise Http404
     if request.method == 'POST':
-        form = forms.CreateArticle(request.POST, request.FILES)
+        form = CreateArticle(request.POST, request.FILES)
         if form.is_valid():
             #save article to db
             instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
-            messages.success(request, ('Articles create'), extra_tags='alert alert-success')
-            return redirect('articles:list')
+            messages.success(request, ('created successfully:)'), extra_tags='alert alert-success')
+            # return redirect('articles:list')
+            return HttpResponseRedirect(instance.get_absolute_url())
     else:
-        form = forms.CreateArticle()
+        form = CreateArticle()
     return render(request, 'articles/article_create.html', {'form':form})
 
-def article_aboutme(request):
-    return render(request, "articles/aboutme.html")
+def edit(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    article = get_object_or_404(Article, id=id)
+    form = EditArticle(request.POST or None, request.FILES or None, instance=article)
+    if form.is_valid():
+        form.save()
+        messages.success(request, ('item edit successfully!'), extra_tags='alert alert-success')
+        return HttpResponseRedirect(article.get_absolute_url())
+        # return redirect('articles:detail', id=id)
+    return render(request, 'articles/edit.html', {'form':form})
 
-def delete(request, slug):
+def delete(request, id):
     if not request.user.is_staff or not request.user.is_superuser:
         raise Http404
     # article = Article.objects.get(id = id)
-    article = get_object_or_404(Article, slug=slug)
+    article = get_object_or_404(Article, id=id)
     if article.delete():
         messages.success(request, ('item deleted successfully!'), extra_tags='alert alert-success')
         return redirect('articles:list')
     # return render(request,'articles/article_list.html', {'article':article})
+
+def article_aboutme(request):
+    return render(request, "articles/aboutme.html")
