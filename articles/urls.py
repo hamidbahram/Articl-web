c73ec4b344421926ade5a807f40c6029aeb5e655
hamid_bh <hamidbahram2@gmail.com>
2019-12-26 13:41:12 +0330

    create url for edit views

--- articles/urls.py
+++ articles/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path(r'create/', views.article_create, name="create"),
     path(r'aboutme/', views.article_aboutme, name="aboutme"),
     path(r'search/', SearchResultsView.as_view(), name="search_results"),
-    re_path(r'(?P<slug>[\w-]+)/', views.article_detail, name="detail"),
-    re_path(r'delete(?P<slug>[\w-]+)', views.delete, name="delete"),
+    re_path(r'(?P<id>[0-9]{1,3})/', views.article_detail, name="detail"),
+    re_path(r'edit/(?P<id>[0-9]{1,3})', views.edit, name='edit'),
+    re_path(r'delete/(?P<id>[0-9]{1,3})', views.delete, name='delete'),
+    # re_path(r'(?P<slug>[\w-]+)/', views.article_detail, name="detail"),
+    # re_path(r'delete(?P<slug>[\w-]+)', views.delete, name="delete"),
 ]
